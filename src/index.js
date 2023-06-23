42c772b45c0a31756102e08b2cc1ad1c46e04b17
vix75god <musarurwamunya@gmail.com>
2023-06-23 01:12:48 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode></React.StrictMode>);
